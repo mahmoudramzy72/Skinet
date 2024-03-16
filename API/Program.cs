78d5e6049b89bd205b9205025cb18e6ce54ea12d
mahmoudramzy72 <mahmoudhamdyramzy72@gmail.com>
2024-03-16 06:01:14 +0200

    End Of Section Three

--- API/Program.cs
+++ API/Program.cs
@@ -1,19 +1,36 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+using Infrastructure.Data;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace API
-{
+{ 
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var LoggerFactory = services.GetRequiredService<ILoggerFactory>();
+                try 
+                {
+                    var context = services.GetRequiredService<StoreContext>();
+                    await context.Database.MigrateAsync();
+                    await StoreContextSeed.SeedAsync(context, LoggerFactory);
+                }
+                catch (Exception ex)
+                {
+                    var Logger = LoggerFactory.CreateLogger<Program>();
+                    Logger.LogError(ex, "An Error Occured During Migration");
+                }   
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
